6f2a2d0c7372d1ac6b9c9390a8f49da13614b47f
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-04-21 19:04:21 -0500

    Cleaning up code

--- dockerman/container.py
+++ dockerman/container.py
@@ -1,14 +1,11 @@
 '''Representation of a Docker container.
 '''
 
-import six
 from functools import partial
+from syn.five import STR
 from syn.base import Base, Attr
 from syn.utils.cmdargs import (Positional, Option, BinaryOption, arglist, 
                                render_args)
-
-if six.PY2:
-    str = unicode
 
 OAttr = partial(Attr, optional=True)
 
@@ -16,14 +13,14 @@ OAttr = partial(Attr, optional=True)
 # Container attributes
 
 container_attrs = \
-dict(image = Attr(str, doc='The image to run'),
-     command = OAttr((str, list), doc='The command to be run in the container'),
+dict(image = Attr(STR, doc='The image to run'),
+     command = OAttr((STR, list), doc='The command to be run in the container'),
      detach = Attr(bool, False, 'Detached mode'),
      tty = Attr(bool, False, 'Allocate a pseudo-TTY'),
      stdin_open = Attr(bool, False, 'Keep STDIN open even if not attached'),
-     name = OAttr(str, doc='A name for the container'),
-     volumes_from = OAttr(list, doc=("List of container names "
-                                     "or Ids to get volumes from")),
+     name = OAttr(STR, doc='A name for the container'),
+     volumes_from = OAttr(list, doc="List of container names "
+                          "or Ids to get volumes from"),
     )
 
 #-------------------------------------------------------------------------------
